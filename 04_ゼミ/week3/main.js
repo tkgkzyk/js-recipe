90881a99ae23b16c7cd95492a68f00ad5c12b64b
tkgkzyk <tkgkzyk1018@gmail.com>
2021-10-15 21:12:26 +0900

    ローカルストレージ

--- 04_ゼミ/week3/main.js
+++ 04_ゼミ/week3/main.js
@@ -4,7 +4,7 @@ const addButton = document.getElementById("add-button")
 const sortButton = document.getElementById("sort-button")
 let memos = []
 
-addButton.onclick = function() {
+/*addButton.onclick = function() {
   memos.push(inputElement.value)
   run()
 }
@@ -23,4 +23,36 @@ const run = function() {
     div.textContent = contents
     container.append(div)
   }
+}*/
+
+addButton.onclick = function() {
+  memos.push(inputElement.value)
+  localStorage.memos = JSON.stringify(memos)
+  run()
+}
+
+sortButton.onclick = function() {
+  memos.sort()
+  if (localStorage.memos) {
+    const memosJson = localStorage.memos
+    memos = JSON.parse(memosJson)
+    run()
+  }
+}
+
+const run = function() {
+  container.innerHTML = ""
+  inputElement.value = ""
+  for (let i = 0; i < memos.length; i++) {
+    const contents = memos[i]
+    const div = document.createElement("div")
+    div.textContent = contents
+    container.append(div)
+  }
+}
+
+if (localStorage.memos) {
+  const memosJson = localStorage.memos
+  memos = JSON.parse(memosJson)
+  run()
 }
